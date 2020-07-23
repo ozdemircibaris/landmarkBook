d828e129344be3d75344f12e6628bbf995244eae
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-07-23 12:06:51 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Baris Ozdemirci on 23.07.2020.
+//  Copyright © 2020 Barış ÖZDEMİRCİ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
